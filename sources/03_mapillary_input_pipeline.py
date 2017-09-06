c9b951a624796fa48318da2028a514abf9e88f33
Raphaël Delhome <raphael.delhome@oslandia.com>
2017-09-06 14:23:39 +0200

    Drop the validation data from the input_pipeline module

--- sources/03_mapillary_input_pipeline.py
+++ sources/03_mapillary_input_pipeline.py
@@ -8,8 +8,6 @@ from tensorflow.python.framework import dtypes
 
 TRAINING_INPUT_PATH = os.path.join("data", "training", "input")
 TRAINING_OUTPUT_PATH = os.path.join("data", "training", "output")
-VALIDATION_INPUT_PATH = os.path.join("data", "validation", "input")
-VALIDATION_OUTPUT_PATH = os.path.join("data", "validation", "output")
 
 IMAGE_HEIGHT  = 2448
 IMAGE_WIDTH   = 3264
@@ -22,42 +20,25 @@ train_filepaths.sort()
 train_filepaths = [os.path.join(TRAINING_INPUT_PATH, fp)
                    for fp in train_filepaths]
 
-validation_filepaths = os.listdir(VALIDATION_INPUT_PATH)
-validation_filepaths.sort()
-validation_filepaths = [os.path.join(VALIDATION_INPUT_PATH, fp)
-                        for fp in validation_filepaths]
-
 # Reading labels
 train_labels = pd.read_csv(os.path.join(TRAINING_OUTPUT_PATH,
                                         "labels.csv")).iloc[:,6:].values
-validation_labels = pd.read_csv(os.path.join(VALIDATION_OUTPUT_PATH,
-                                             "labels.csv")).iloc[:,6:].values
 
 # Convert string file paths and integer labels into tensors
 train_images = ops.convert_to_tensor(train_filepaths, dtype=tf.string)
 train_labels = ops.convert_to_tensor(train_labels, dtype=tf.int16)
-validation_images = ops.convert_to_tensor(validation_filepaths, dtype=tf.string)
-validation_labels = ops.convert_to_tensor(validation_labels, dtype=tf.int16)
 
 # Create input queues
 train_input_queue = tf.train.slice_input_producer([train_images, train_labels],
                                                   shuffle=False)
-validation_input_queue = tf.train.slice_input_producer([validation_images,
-                                                        validation_labels],
-                                                       shuffle=False)
 
 # Process path and string tensor into an image and a label
 file_content = tf.read_file(train_input_queue[0])
 train_image = tf.image.decode_jpeg(file_content, channels=NUM_CHANNELS)
 train_label = train_input_queue[1]
 
-file_content = tf.read_file(validation_input_queue[0])
-validation_image = tf.image.decode_jpeg(file_content, channels=NUM_CHANNELS)
-validation_label = validation_input_queue[1]
-
 # Define tensor shape
 train_image.set_shape([IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS])
-validation_image.set_shape([IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS])
 
 # Collect batches of images before processing
 train_image_batch, train_label_batch, train_filename_batch = tf.train.batch(
@@ -67,9 +48,6 @@ train_image_batch, train_label_batch, train_filename_batch = tf.train.batch(
     batch_size=BATCH_SIZE,
     num_threads=4
 )
-validation_image_batch, validation_label_batch, validation_filename_batch =\
-tf.train.batch([validation_image, validation_label, validation_input_queue[0]],
-               batch_size=BATCH_SIZE, num_threads=4)
 
 with tf.Session() as sess:
   # Initialize the variables
@@ -78,11 +56,12 @@ with tf.Session() as sess:
   coord = tf.train.Coordinator()
   threads = tf.train.start_queue_runners(coord=coord)
   print("From the train set:")
-  for i in range(10):
-    print(sess.run(train_filename_batch))
-  print("From the validation set:")
-  for i in range(5):
-    print(sess.run(validation_filename_batch))
+  for i in range(2):
+    b_filenames, b_images, b_labels = sess.run([train_filename_batch,
+                                                train_image_batch,
+                                                train_label_batch])
+    print(b_filenames)
+    print(b_labels)
   # Stop our queue threads
   coord.request_stop()
   coord.join(threads)
