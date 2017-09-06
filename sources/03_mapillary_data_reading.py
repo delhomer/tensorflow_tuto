c79e4f0965e68a51e6004cc1987701cca07124a5
Raphaël Delhome <raphael.delhome@oslandia.com>
2017-09-06 19:14:11 +0200

    fixup! Reorganize the sources by moving function into a dedicated utils.py module

--- sources/03_mapillary_data_reading.py
+++ sources/03_mapillary_data_reading.py
@@ -9,20 +9,6 @@ import sys
 
 import utils
 
-DATASET = ["training", "validation", "testing"]
-IMG_SIZE = (816, 612)
-IMAGE_TYPES = ["images", "instances", "labels"]
-TRAINING_IMAGE_PATH = os.path.join("data", "training", "images")
-TRAINING_LABEL_PATH = os.path.join("data", "training", "labels")
-TRAINING_INPUT_PATH = os.path.join("data", "training", "input",
-                                   "{}_{}".format(IMG_SIZE[0], IMG_SIZE[1]))
-TRAINING_OUTPUT_PATH = os.path.join("data", "training", "output")
-VALIDATION_IMAGE_PATH = os.path.join("data", "validation", "images")
-VALIDATION_LABEL_PATH = os.path.join("data", "validation", "labels")
-VALIDATION_INPUT_PATH = os.path.join("data", "validation", "input",
-                                   "{}_{}".format(IMG_SIZE[0], IMG_SIZE[1]))
-VALIDATION_OUTPUT_PATH = os.path.join("data", "validation", "output")
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
--- sources/utils.py
+++ sources/utils.py
@@ -17,6 +17,19 @@ ch = logging.StreamHandler(sys.stdout)
 ch.setFormatter(format)
 logger.addHandler(ch)
 
+DATASET = ["training", "validation", "testing"]
+IMG_SIZE = (816, 612)
+IMAGE_TYPES = ["images", "instances", "labels"]
+TRAINING_IMAGE_PATH = os.path.join("data", "training", "images")
+TRAINING_LABEL_PATH = os.path.join("data", "training", "labels")
+TRAINING_INPUT_PATH = os.path.join("data", "training", "input",
+                                   "{}_{}".format(IMG_SIZE[0], IMG_SIZE[1]))
+TRAINING_OUTPUT_PATH = os.path.join("data", "training", "output")
+VALIDATION_IMAGE_PATH = os.path.join("data", "validation", "images")
+VALIDATION_LABEL_PATH = os.path.join("data", "validation", "labels")
+VALIDATION_INPUT_PATH = os.path.join("data", "validation", "input",
+                                   "{}_{}".format(IMG_SIZE[0], IMG_SIZE[1]))
+VALIDATION_OUTPUT_PATH = os.path.join("data", "validation", "output")
 
 def make_dir(path):
     """ Create a directory if there isn't one already. """
