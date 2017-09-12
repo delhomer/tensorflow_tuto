c2590f51d68e1dc908641316c6fa169cef15476a
Raphaël Delhome <raphael.delhome@oslandia.com>
2017-09-12 11:31:36 +0200

    Put the layer declaration code in dedicated functions

--- sources/03_convolutional_neural_network_mapillary.py
+++ sources/03_convolutional_neural_network_mapillary.py
@@ -124,81 +124,72 @@ with tf.name_scope("data"):
 
 # Step 5: model building
 
-# The model is composed of the following steps:
-# conv -> relu -> pool -> conv -> relu -> pool -> fully connected -> sigmoid
-# - conv: convolution between an input neuron and an image filter
-# - relu (REctified Linear Unit): neuron activation function
-# - pool: max pooling layer, that considers the maximal value in a n*n patch
-# - fully connected: full connection between two consecutive neuron layer, concretized by a matrix multiplication
-# - sigmoid: neuron activation function, associated with output (multilabel
-# classification problem)
+CONV_LAYER_COUNTER = 0
+POOL_LAYER_COUNTER = 0
+FULLCON_LAYER_COUNTER = 0
 
-# They represent its structure, and may be showed within graph with tensorboard
-# command.
+# Convolutional layer
+def conv_layer(input_layer, input_layer_depth, kernel_dim, layer_depth, conv_strides, counter):
+    counter = counter + 1
+    with tf.variable_scope('conv'+str(counter)) as scope:
+        # Create kernel variable of dimension [K_C1, K_C1, NUM_CHANNELS, L_C1]
+        kernel = tf.get_variable('kernel',
+                                 [kernel_dim, kernel_dim,
+                                  input_layer_depth, layer_depth],
+                                 initializer=tf.truncated_normal_initializer())
+        # Create biases variable of dimension [L_C1]
+        biases = tf.get_variable('biases',
+                                 [layer_depth],
+                                 initializer=tf.constant_initializer(0.0))
+        # Apply the image convolution
+        conv = tf.nn.conv2d(input_layer, kernel, strides=conv_strides,
+                            padding='SAME')
+        # Apply relu on the sum of convolution output and biases
+        # Output is of dimension BATCH_SIZE * IMAGE_HEIGHT * IMAGE_WIDTH * L_C1.
+        return tf.nn.relu(tf.add(conv, biases), name=scope.name), counter
 
-# First convolutional layer
-
-with tf.variable_scope('conv1') as scope:
-    # Create kernel variable of dimension [K_C1, K_C1, NUM_CHANNELS, L_C1]
-    kernel = tf.get_variable('kernel',
-                             [K_C1, K_C1, NUM_CHANNELS, L_C1],
-                             initializer=tf.truncated_normal_initializer())
-    # Create biases variable of dimension [L_C1]
-    biases = tf.get_variable('biases',
-                             [L_C1],
-                             initializer=tf.constant_initializer(0.0))
-    # Apply the image convolution
-    conv = tf.nn.conv2d(X, kernel, strides=STR_C1, padding='SAME')
-    # Apply relu on the sum of convolution output and biases
-    conv1 = tf.nn.relu(tf.add(conv, biases), name=scope.name)
-    # Output is of dimension BATCH_SIZE * IMAGE_HEIGHT * IMAGE_WIDTH * L_C1.
-
-# First pooling layer
-
-with tf.variable_scope('pool1') as scope:
-    # Apply max pooling
-    pool1 = tf.nn.max_pool(conv1, ksize=KS_P1, strides=STR_P1, padding='SAME')
-    # Output is of dimension BATCH_SIZE x 612 x 816 x L_C1
-
-# Second convolutional layer
-
-with tf.variable_scope('conv2') as scope:
-    # Similar to conv1, except kernel now is of the size 5 x 5 x L_C1 x L_C2
-    kernel = tf.get_variable('kernels', [K_C2, K_C2, L_C1, L_C2], 
-                        initializer=tf.truncated_normal_initializer())
-    biases = tf.get_variable('biases', [L_C2],
-                        initializer=tf.random_normal_initializer())
-    conv = tf.nn.conv2d(pool1, kernel, strides=STR_C2, padding='SAME')
-    conv2 = tf.nn.relu(tf.add(conv, biases), name=scope.name)
-    # Output is of dimension BATCH_SIZE x 612 x 816 x L_C2
-
-# Second pooling layer
-
-with tf.variable_scope('pool2') as scope:
-    # Similar to pool1
-    pool2 = tf.nn.max_pool(conv2, ksize=KS_P2, strides=STR_P2, padding='SAME')
-    # Output is of dimension BATCH_SIZE x 153 x 204 x L_C2
+# Max-pooling layer
+def maxpool_layer(input_layer, pool_ksize, pool_strides, counter):
+    counter = counter + 1
+    with tf.variable_scope('pool'+str(counter)) as scope:
+        return tf.nn.max_pool(input_layer, ksize=pool_ksize,
+                               strides=pool_strides, padding='SAME'), counter
+        # Output is of dimension BATCH_SIZE x 612 x 816 x L_C1
 
 # Fully-connected layer
+def reshape(height, width, str_c1, str_p1, str_c2, str_p2, last_layer_depth):
+    new_height = int(height / (str_c1[2]*str_p1[2]*str_c2[2]*str_p2[2]))
+    new_width = int(width / (str_c1[1]*str_p1[1]*str_c2[1]*str_p2[1]))
+    return new_height * new_width * last_layer_depth
 
-with tf.variable_scope('reshaping') as scope:
-    # Reshape pool2 to 2 dimensional
-    new_height = int(IMAGE_HEIGHT / (STR_C1[2]*STR_P1[2]*STR_C2[2]*STR_P2[2]))
-    new_width = int(IMAGE_WIDTH / (STR_C1[1]*STR_P1[1]*STR_C2[1]*STR_P2[1]))
-    input_features = new_height * new_width * L_C2
-    reshaped = tf.reshape(pool2, [-1, input_features])
+def fullconn_layer(input_layer, height, width, str_c1, str_p1, str_c2, str_p2,
+                   last_layer_depth, fc_layer_depth, counter):
+    counter = counter + 1
+    with tf.variable_scope('fc'+str(counter)) as scope:
+        fc_size = reshape(height, width, str_c1, str_p1, str_c2, str_p2,
+                          last_layer_depth)
+        reshaped = tf.reshape(input_layer, [-1, fc_size])
+        # Create weights and biases
+        w = tf.get_variable('weights', [fc_size, fc_layer_depth],
+                            initializer=tf.truncated_normal_initializer())
+        b = tf.get_variable('biases', [fc_layer_depth],
+                            initializer=tf.constant_initializer(0.0))
+        # Apply relu on matmul of reshaped and w + b
+        fc = tf.nn.relu(tf.add(tf.matmul(reshaped, w), b), name='relu')
+        # Apply dropout
+        return tf.nn.dropout(fc, dropout, name='relu_with_dropout'), counter
 
-with tf.variable_scope('fc') as scope:
-    # Create weights and biases
-    w = tf.get_variable('weights', [input_features, L_FC],
-                        initializer=tf.truncated_normal_initializer())
-    b = tf.get_variable('biases', [L_FC],
-                        initializer=tf.constant_initializer(0.0))
-    # Apply relu on matmul of reshaped and w + b
-    fc = tf.nn.relu(tf.add(tf.matmul(reshaped, w), b), name='relu')
-    # Apply dropout
-    dropout = tf.placeholder(tf.float32, name='dropout')
-    fc = tf.nn.dropout(fc, dropout, name='relu_dropout')
+conv1, CONV_LAYER_COUNTER = conv_layer(X, NUM_CHANNELS, K_C1, L_C1, STR_C1,
+                                       CONV_LAYER_COUNTER)
+pool1, POOL_LAYER_COUNTER = maxpool_layer(conv1, KS_P1, STR_P1,
+                                          POOL_LAYER_COUNTER)
+conv2, CONV_LAYER_COUNTER = conv_layer(pool1, L_C1, K_C2, L_C2, STR_C2,
+                                       CONV_LAYER_COUNTER)
+pool2, POOL_LAYER_COUNTER = maxpool_layer(conv2, KS_P2, STR_P2,
+                                          POOL_LAYER_COUNTER)
+fc1, FULLCON_LAYER_COUNTER = fullconn_layer(pool2, IMAGE_HEIGHT, IMAGE_WIDTH,
+                                            STR_C1, STR_P1, STR_C2, STR_P2,
+                                            L_C2, L_FC, FULLCON_LAYER_COUNTER)
 
 # Output building
 
